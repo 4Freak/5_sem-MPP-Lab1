818a1306dbf6e27dee280b31afbae027da8dcae0
4Freak <pasha.bobrik@bk.ru>
2022-09-12 00:46:41 +0300

    not first commit

---
+++ Tracer/Tracer/Tracer.Core/TraceThread.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Collections.Concurrent;
+
+namespace Tracer.Core
+{
+	public class TraceThread
+	{
+		public int id;
+		public Stopwatch time;
+		public ConcurrentDictionary<int, TraceMethod> innerMethods;
+	}
+}
